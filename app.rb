b755e3e681ab4c04eca385802c9ab923ec2fa3fe
EC2 Default User <ec2-user@ip-10-1-8-251.ap-northeast-1.compute.internal>
2024-01-17 10:01:49 +0000

    second

--- app.rb
+++ app.rb
@@ -2,15 +2,20 @@ require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
 require './models.rb'
-
+require 'line/bot'
 get '/' do
   erb :index
 end
 
-class LinebotController < ApplicationController
-  require 'line/bot'
+def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = "a9a7928fd25356ca14b0e0aa05b6568c"
+      config.channel_token = "uOSv2yphg2AkqPJOKJd6et3jVEA+YTwlKUflGvCikdDW3T81UBiOtsnkfGfmZps4uoaL2HPn4yha2CnidLe8cTHv1xLINVqDAVlBWcUqIof98V/SFHG5ShXxTxrd2/lXhypHaUfMsm2AXeZTtDCWUAdB04t89/1O/w1cDnyilFU="
+    }
+end
 
-  def callback
+ 
+post '/callback'
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
@@ -32,15 +37,10 @@ class LinebotController < ApplicationController
       client.reply_message(event['replyToken'], message)
     end
     head :ok
-  end
 
-private
+
+
 
 # LINE Developers登録完了後に作成される環境変数の認証
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
-end
+ 
+
